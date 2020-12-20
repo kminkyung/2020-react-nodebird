9b11b9bfeccdc986a86954c93f3c03ee890cd39d
Minkyung Kim <mminkyung.k@gmail.com>
2020-12-20 19:04:13 +0900

    Apply react-slick

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -2,8 +2,13 @@ import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import {PlusOutlined} from "@ant-design/icons";
 
+import ImagesZoom from './ImagesZoom';
+
 const PostImages = ({images}) => {
   const [showImagesZoom, setShowImagesZoom] = useState(true);
+  const onClose = useCallback(() => {
+
+  }, []);
 
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
@@ -13,6 +18,7 @@ const PostImages = ({images}) => {
     return (
       <>
         <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
       </>
     )
   }
@@ -21,6 +27,7 @@ const PostImages = ({images}) => {
       <>
         <img role="presentation" style={{width: "50%", display: 'inline-block'}} src={images[0].src} alt={images[0].src} onClick={onZoom}/>
         <img role="presentation" style={{width: "50%", display: 'inline-block'}} src={images[1].src} alt={images[1].src} onClick={onZoom}/>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
       </>
     )
   }
@@ -39,6 +46,7 @@ const PostImages = ({images}) => {
           개의 사진 더보기
         </div>
       </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose}/>}
     </>
   );
 };
